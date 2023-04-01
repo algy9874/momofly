7c62377ead5da76e2c50f5096c91388e882f3bb4
algy9874 <algy9874@gmail.com>
2023-04-01 15:39:06 +0800

    1

---
+++ js/order.js
@@ -0,0 +1,51 @@
+$(document).on('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const inputValue = urlParams.get('product-link');
+    if(inputValue != null)
+        document.getElementById("oc-product-link").value = inputValue;
+    // document.getElementById("oc-user-id").value += localStorage.getItem("fyp-user-id");
+    // document.getElementById("oc-email").value += localStorage.getItem("fyp-email");
+});
+$(document).on("input", "#oc-product-quantity", function() {
+    if (this.value.length > 3 || this.value > 0) {
+      this.value = this.value.slice(0,3);
+    }else {
+        this.value = "";
+    }
+});
+$(document).on("click","#order-information-btn", function(){
+    let recordList = [
+        document.getElementById("oc-product-link"),
+        document.getElementById("oc-product-name"),
+        document.getElementById("oc-product-type"),
+        document.getElementById("oc-product-quantity"),
+        document.getElementById("oc-product-price")
+    ]
+    if(checkNotNull(recordList)){
+        changePage("#order-information", "#order-detali");
+    }
+});
+$(document).on("click","#order-detali-btn", function(){
+    //之後再檢查
+    changePage("#order-detali", "#order-about");
+});
+$(document).on("click","#order-information-back", function(){
+    changePage("#order-detali", "#order-information");
+});
+$(document).on("click","#order-detali-back", function(){
+    changePage("#order-about", "#order-detali");
+});
+function checkNotNull(arrayList){
+    for(var i = 0; i < arrayList.length; i ++){
+        if(arrayList[i].value.length <= 0){
+            arrayList[i].focus();
+            return false;
+        }
+    }
+    return true;
+}
+function changePage(currentPageId, nextPageId){
+    $(currentPageId).css({'display': 'none'});
+    $(nextPageId).delay(100).fadeIn();
+
+}
